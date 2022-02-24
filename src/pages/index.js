ae6610b484410b3f319a2997ce1ca3319458f75f
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-25 01:17:58 +0530

    Products

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,32 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import ProductFeed from '../components/ProductFeed'
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className='bg-gray-100'>
       <Head>
         <title>Amazon</title>
       </Head>
-
       <Header />
 
       <main className='max-w-screen-2xl mx-auto'>
         <Banner />
+        <ProductFeed products={products} />
       </main>
     </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    res => res.json()
+  )
+
+  return {
+    props: {
+      products
+    }
+  }
 }
